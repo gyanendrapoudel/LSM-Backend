667b26908df83fce2a345512cbc5ba05eb03710f
Gyanendra <gyanen56@gmail.com>
2024-12-24 10:35:08 +1100

    First get request

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log("First")
+import express from "express"
+const app = express()
+
+// Reading env variable
+import dotenv from 'dotenv'
+dotenv.config()
+
+const PORT = process.env.PORT || 8000
+
+app.use('/user', (req, res) => {
+  res.json({
+    message: 'hello from router',
+  })
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`app is listening on ${PORT} `)
+})
