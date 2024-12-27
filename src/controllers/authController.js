5b7aa8b5eed763871732fe6f7b4da5d80d7acef0
Gyanendra <gyanen56@gmail.com>
2024-12-27 11:29:09 +1100

    password hashing using bycrpt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,12 @@
 import { createUser } from "../models/user/UserModel.js"
+import { hashPassword } from "../utils/bcrypt.js"
 
 
 export const insertNewUser = async(req,res,error)=>{
     try {
+    const{password} = req.body
+    // password encryption
+    req.body.password = hashPassword(password)
     const user = await createUser(req.body)
     user?.id
       ? res.json({
